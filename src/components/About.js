9830c07b34db6f3eac4016d196413efe4c2413b9
Thomas Sankara <thomassankara74@gmail.com>
2021-03-17 18:18:48 +0300

    About component

---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <>
+      <section id="about">
+        <h2 className="text-center text-4xl mt-10 mb-4">About me</h2>
+        <p className="text-sm text-center px-5 md:w-2/3 md:mx-auto md:text-base lg:text-lg">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quis
+          dolorem nisi dolores, error porro quam explicabo sed? Molestiae est
+          quibusdam itaque voluptate eos nemo quasi quis quas asperiores at
+          pariatur quia earum, corrupti dicta animi consectetur vero fugiat
+          magni molestias eveniet quos culpa quae. Tempora cupiditate laudantium
+          optio dignissimos?
+        </p>
+      </section>
+    </>
+  )
+}
+
+export default About
