b36045eac0d825e9b2672d55db4e03bd6a308656
Thomas Sankara <thomassankara74@gmail.com>
2021-03-17 18:20:46 +0300

    render components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Projects from "./components/Projects"
+import Showcase from "./components/Showcase"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Showcase />
+      <About />
+      <Projects />
+      <Footer />
+    </>
+  )
+}
+
+export default App
