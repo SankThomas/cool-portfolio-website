768658c11baba48a578cce91380b3c4a905d3658
Thomas Sankara <thomassankara74@gmail.com>
2021-03-17 18:20:20 +0300

    navbar data array

---
+++ src/data/navbar.js
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const navbar = [
+  {
+    id: uuidv4(),
+    title: "Home",
+    url: "#home",
+  },
+  {
+    id: uuidv4(),
+    title: "About",
+    url: "#about",
+  },
+  {
+    id: uuidv4(),
+    title: "Projects",
+    url: "#projects",
+  },
+  {
+    id: uuidv4(),
+    title: "Contacts",
+    url: "#contacts",
+  },
+]
