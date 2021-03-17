19d6e18473a2e4485b4e028cd329acb10a8b1dc0
Thomas Sankara <thomassankara74@gmail.com>
2021-03-17 18:20:31 +0300

    projects data array

---
+++ src/data/projects.js
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from "uuid"
+
+export const projects = [
+  {
+    id: uuidv4(),
+    title: "News Articles App",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, natus? Cum aliquid vitae numquam similique.",
+    tech: ["ReactJs", "Tailwind CSS", "Netlify"],
+    image: "./assets/image-1.jpeg",
+  },
+  {
+    id: uuidv4(),
+    title: "Book Catalogue App",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, natus? Cum aliquid vitae numquam similique.",
+    tech: ["ReactJs", "Tailwind CSS", "Netlify"],
+    image: "./assets/image-2.jpeg",
+  },
+  {
+    id: uuidv4(),
+    title: "Covid-19 App",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, natus? Cum aliquid vitae numquam similique.",
+    tech: ["ReactJs", "Tailwind CSS", "Netlify"],
+    image: "./assets/image-3.jpeg",
+  },
+  {
+    id: uuidv4(),
+    title: "Portfolio Website",
+    desc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, natus? Cum aliquid vitae numquam similique.",
+    tech: ["ReactJs", "Tailwind CSS", "Netlify"],
+    image: "./assets/image-4.jpeg",
+  },
+]
